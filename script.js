1cd02ff78d386346ea1ec36caae03d9a1de4ad04
julius <juliusnjenga20002@gmail.com>
2026-02-16 20:27:43 +0300

    final product

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ const display_earning = document.querySelector('#display_earning');
 
 form.addEventListener('submit' , (e)=>{
     e.preventDefault();
-    if(!/\d/.test(salary.value) || !/\d/.test(allowance.value) || !/\d/.test(deduction.value)){
-        alert('Can only contain numeric values');
-        salary.value = '';
-        allowance.value = '';
-        deduction.value = '';
-        return;
-    }
     calculate();
 })
+
 
 
 function calculate(){
     let get_salary = parseFloat(salary.value.trim());
     let get_allowance = parseFloat(allowance.value.trim());
     let get_deduction = parseFloat(deduction.value.trim());
-    let total = get_salary + get_allowance + get_deduction;
+    if (Number.isNaN(get_salary) || Number.isNaN(get_allowance) ||Number.isNaN(get_deduction)){
+        alert("Can only contain numbers");
+        salary.value = '';
+        allowance.value = '';
+        deduction.value = '';
+        return;
+    }
+    let total = (get_salary + get_allowance) - get_deduction;
     display_earning.textContent = total.toFixed(2);
     salary.value = '';
     allowance.value = '';
